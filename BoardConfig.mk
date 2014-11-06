59194732562b03600039f9ceb8a6187b8accb61a
andixlm <andixev@gmail.com>
2014-11-06 15:17:00 +0500

    Revert "BoardConfig.mk: move screen size to common repo"
    This reverts commit 56add91d86c8830cfa4645de980dc120a61aa5c2.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,10 @@ TARGET_KERNEL_SOURCE := kernel/samsung/s2ve
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/s2ve/bluetooth
 
+# Boot animation
+TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 800
+
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/s2ve/ramdisk/fstab.capri_ss_s2ve
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
