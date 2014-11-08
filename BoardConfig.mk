6b401d06c0ffa00f904ed34afa6c7644a575cf6d
andixlm <andixev@gmail.com>
2014-11-08 20:22:43 +0500

    Move left common parts to a common repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/s2ve/bluetooth
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/s2ve/ramdisk/fstab.capri_ss_s2ve
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
 
 # inherit from the proprietary version
 -include vendor/samsung/bcm281x5-common/BoardConfigVendor.mk
--- s2ve.mk
+++ s2ve.mk
@@ -11,7 +11,6 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # Init files
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/ramdisk/init.capri_ss_s2ve.rc:root/init.capri_ss_s2ve.rc \
-	$(LOCAL_PATH)/ramdisk/init.bcm281x5.usb.rc:root/init.bcm281x5.usb.rc \
 	$(LOCAL_PATH)/ramdisk/init.recovery.capri_ss_s2ve.rc:root/init.recovery.capri_ss_s2ve.rc \
 	$(LOCAL_PATH)/ramdisk/ueventd.capri_ss_s2ve.rc:root/ueventd.capri_ss_s2ve.rc \
 	$(LOCAL_PATH)/ramdisk/fstab.capri_ss_s2ve:root/fstab.capri_ss_s2ve
