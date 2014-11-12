2662773b935d51d1736d98f94668880b5ef570ba
andixlm <andixev@gmail.com>
2014-11-12 20:42:18 +0500

    Update common repos' names to galaxys2plus-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,8 @@
 # Include common BoardConfig
--include device/samsung/bcm281x5-common/BoardConfigCommon.mk
+-include device/samsung/galaxys2plus-common/BoardConfigCommon.mk
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/bcm281x5-common
+TARGET_KERNEL_SOURCE := kernel/samsung/galaxys2plus-common
 TARGET_KERNEL_CONFIG := cyanogenmod_s2ve_defconfig
 
 # Bluetooth
@@ -12,4 +12,4 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9105/bluetooth
 TARGET_RECOVERY_FSTAB := device/samsung/i9105/ramdisk/fstab.capri_ss_s2ve
 
 # inherit from the proprietary version
--include vendor/samsung/bcm281x5-common/BoardConfigVendor.mk
+-include vendor/samsung/galaxys2plus-common/BoardConfigVendor.mk
