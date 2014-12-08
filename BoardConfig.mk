38e3f60a1365c50fc772dbd0c2fc2c93f9a989b8
andixlm <andixev@gmail.com>
2014-12-08 23:04:15 +0500

    Clean up makefiles

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,11 @@
 # Include common BoardConfig
 -include device/samsung/galaxys2plus-common/BoardConfigCommon.mk
 
-# Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_s2ve_defconfig
-
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9105/bluetooth
+
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_s2ve_defconfig
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/i9105/ramdisk/fstab.capri_ss_s2ve
