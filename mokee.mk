d79ea7823b36da0d1f5bc958145eea1258c7972d
oned46 <carloalberto_oneda@outlook.it>
2015-03-04 15:04:18 +0100

    mokee changes

--- mokee.mk
+++ mokee.mk
@@ -1,9 +1,9 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/i9105/full_i9105.mk)
 
 PRODUCT_DEVICE := i9105
-PRODUCT_NAME := cm_i9105
+PRODUCT_NAME := mk_i9105
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=s2vexx \
